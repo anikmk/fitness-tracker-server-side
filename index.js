73802499403608cb88239e5f87288be246f34e81
anikmk <anikmollik122321@gmail.com>
2023-11-28 15:16:37 +0600

    access admin

--- index.js
+++ index.js
@@ -35,6 +35,10 @@ async function run() {
     const entryForumCollection = client.db("fitnessDb").collection("entryForum");
 
     // user related api
+    app.get('/users',async(req,res)=>{
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    })
     app.post('/users',async(req,res)=>{
       const user = req.body;
       const query = {email: user.email};
@@ -45,6 +49,39 @@ async function run() {
       const result = await userCollection.insertOne(user);
       res.send(result);
     })
+
+    // app.patch('/users/admin/:id', async(req,res)=>{
+    //   const id = req.params.id;
+    //   const filter = {_id: new ObjectId(id)}
+    //   const updatedDoc = {
+    //     $set: {
+    //       role:'admin'
+    //     }
+    //   }
+    //   const result = await userCollection.updateOne(filter,updatedDoc);
+    //   res.send(result);
+    // })
+    // Check if a user has admin role route
+    app.get("/users/:email", async (req, res) => {
+      try {
+        const userEmail = req.params.email;
+    
+        const user = await userCollection.findOne({ email: userEmail });
+    
+        if (!user) {
+          return res.status(404).send({ error: "User not found" });
+        }
+    
+        const userRole = user.role || "user"; // Assuming 'role' is a string property in the user object
+        const isAdmin = userRole === "admin";
+        const isCoach = userRole === "trainer";
+    
+        res.status(200).send({ email: userEmail, isAdmin, isCoach });
+      } catch (error) {
+        console.error(error);
+        res.status(500).send({ error: "Internal Server Error" });
+      }
+    });
 
     // entry class related api
     app.get('/entryclass',async(req,res)=>{
@@ -63,12 +100,32 @@ async function run() {
         res.send(result);
     })
     // entry forum related api
+    app.get('/entryforum',async(req,res)=>{
+      const result = await entryForumCollection.find().toArray();
+      res.send(result);
+    })
     app.post('/entryforum',async(req,res)=>{
       const entryForumInfo = req.body;
       const result = await entryForumCollection.insertOne(entryForumInfo);
       res.send(result);
     })
-
+    app.patch('/entryforum/:id',async(req,res)=>{
+      const item = req.body;
+      const currentLike = item.like + 1;
+      const currentDislike = item.dislike + 1;
+      console.log(item)
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const option = { upsert: true };
+      const updatedDoc = {
+        $set:{
+          like:currentLike,
+          dislike:currentDislike
+        }}
+        const result = await entryForumCollection.updateOne( filter, updatedDoc,option)
+        res.send(result);
+      })
+    
     // trainer releted apis
     app.get('/trainer',async(req,res)=>{
         const result = await trainerCollection.find().toArray();
